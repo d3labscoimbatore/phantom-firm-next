11c78c29a53887e882d8e8217690fd001267b777
d3labscoimbatore <d3labscoimbatore@gmail.com>
2024-02-04 13:44:37 +0530

    say hello buttin removed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,18 +4,13 @@ import logo from "../../../public/logo.jpg";
 import Image from "next/image";
 import Link from "next/link";
 import { IoMenu, IoClose } from "react-icons/io5";
-import { useRef, useState, useEffect } from "react";
+import {  useState } from "react";
 import "../header/header.css";
 
 const Header = ({ headerSection }) => {
   const { navigationMenu, navigationLinks, lottiePlayerURL } = headerSection;
 
   const [isbuttonClicked, setIsbuttonClicked] = useState(false);
-
-  const ref = useRef(null);
-  useEffect(() => {
-    import("@lottiefiles/lottie-player");
-  });
 
   return (
     <header className="headerContainer">
@@ -26,7 +21,7 @@ const Header = ({ headerSection }) => {
           </Link>
         </div>
         <div>
-          <ul className={`menus`}>
+          <ul className={`menus ${isbuttonClicked ? "menusActive" : ""}`}>
             <li>
               <Link
                 href={navigationLinks[0]}
@@ -51,27 +46,7 @@ const Header = ({ headerSection }) => {
                 {navigationMenu[2]}
               </Link>
             </li>
-            <li>
-              <Link
-                href={navigationLinks[3]}
-                onClick={() => setIsbuttonClicked(!isbuttonClicked)}
-              >
-                <div className="contact lastItem">
-                  <lottie-player
-                    ref={ref}
-                    autoplay
-                    loop
-                    mode="normal"
-                    src={lottiePlayerURL}
-                    style={{
-                      width: "22rem",
-                      height: "max-content",
-                    }}
-                  ></lottie-player>
-                  <span className="contactText"> {navigationMenu[3]} </span>
-                </div>
-              </Link>
-            </li>
+           
           </ul>
 
           {/* Hamburger Menu for Mobile Device  */}
