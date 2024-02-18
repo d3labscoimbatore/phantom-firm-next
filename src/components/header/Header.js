a0d29d4708358972f99f919479196bedfe618734
d3labscoimbatore <d3labscoimbatore@gmail.com>
2024-02-18 14:38:41 +0530

    section added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
 import logo from "../../../public/logo.jpg";
 import Image from "next/image";
 import Link from "next/link";
 import { IoMenu, IoClose } from "react-icons/io5";
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import "../header/header.css";
 
 const Header = ({ headerSection }) => {
@@ -12,6 +12,10 @@ const Header = ({ headerSection }) => {
 
   const [isbuttonClicked, setIsbuttonClicked] = useState(false);
 
+  const ref = useRef(null);
+  useEffect(() => {
+    import("@lottiefiles/lottie-player");
+  });
 
   return (
     <header className="headerContainer">
@@ -24,21 +28,32 @@ const Header = ({ headerSection }) => {
         <div>
           <ul className={`menus ${isbuttonClicked ? "menusActive" : ""}`}>
             <li onClick={() => setIsbuttonClicked(!isbuttonClicked)}>
-                <Link href="#solutions">
-                  <p>{navigationMenu[0]}</p>
-                </Link>
+                <Link href="#solutions">{navigationMenu[0]}</Link>
             </li>
             <li onClick={() => setIsbuttonClicked(!isbuttonClicked)}>
-                <Link href="#about">
-                  <p>{navigationMenu[1]}</p>
-                </Link>
+                <Link href="#about">{navigationMenu[1]}</Link>
             </li>
             <li onClick={() => setIsbuttonClicked(!isbuttonClicked)}>
-                <Link href="#faq">
-                  <p>{navigationMenu[2]}</p>
+                <Link href="#faq">{navigationMenu[2]}</Link>
+            </li>
+            <li onClick={() => setIsbuttonClicked(!isbuttonClicked)}>
+                <Link href="#contact">
+                  <div className="contact lastItem">
+                    <lottie-player
+                      ref={ref}
+                      autoplay
+                      loop
+                      mode="normal"
+                      src={lottiePlayerURL}
+                      style={{
+                        width: "22rem",
+                        height: "max-content",
+                      }}
+                    ></lottie-player>
+                    <span className="contactText"> {navigationMenu[3]} </span>
+                  </div>
                 </Link>
             </li>
-           
           </ul>
 
           {/* Hamburger Menu for Mobile Device  */}
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,13 +1,18 @@
-
+"use client";
+import React, { useRef } from "react";
 import "../hero/hero.css";
 
 const Hero = ({ heroSection }) => {
-  const { title, subTitle } = heroSection;
+  const { title, subTitle, lottiePlayerURL } = heroSection;
 
   const firstTitle = title.substring(0, 19);
   const secondTitle = title.substring(20, 33);
   const thirdTitle = title.substring(34, 41);
 
+  const ref = useRef(null);
+  React.useEffect(() => {
+    import("@lottiefiles/lottie-player");
+  });
 
   return (
     <div className="heroContainer">
@@ -23,7 +28,15 @@ const Hero = ({ heroSection }) => {
           <h3 className="shortDescription">{subTitle}</h3>
         </div>
         <div className="heroRight">
-
+          <lottie-player
+            ref={ref}
+            autoplay
+            loop
+            src={lottiePlayerURL}
+            speed="1"
+            style={{ width: "50rem", height: "50rem", marginTop: "-5rem" }}
+            debug
+          ></lottie-player>
         </div>
       </div>
     </div>
