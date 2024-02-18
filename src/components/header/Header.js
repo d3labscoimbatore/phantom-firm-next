ec8d540b11049baa38779340a5cef3d327447759
d3labscoimbatore <d3labscoimbatore@gmail.com>
2024-02-18 12:27:50 +0530

    lottie removed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,18 +4,14 @@ import logo from "../../../public/logo.jpg";
 import Image from "next/image";
 import Link from "next/link";
 import { IoMenu, IoClose } from "react-icons/io5";
-import { useRef, useState, useEffect } from "react";
+import { useState } from "react";
 import "../header/header.css";
 
 const Header = ({ headerSection }) => {
-  const { navigationMenu, navigationLinks, lottiePlayerURL } = headerSection;
+  const { navigationMenu } = headerSection;
 
   const [isbuttonClicked, setIsbuttonClicked] = useState(false);
 
-  const ref = useRef(null);
-  useEffect(() => {
-    import("@lottiefiles/lottie-player");
-  });
 
   return (
     <header className="headerContainer">
@@ -51,27 +47,7 @@ const Header = ({ headerSection }) => {
                 {navigationMenu[2]}
               </Link>
             </li>
-            <li>
-              <Link
-                href=  "/#contact"
-                onClick={() => setIsbuttonClicked(!isbuttonClicked)}
-              >
-                <div className="contact lastItem">
-                  <lottie-player
-                  ref={ref}
-                    autoplay
-                    loop
-                    mode="normal"
-                    src={lottiePlayerURL}
-                    style={{
-                      width: "22rem",
-                      height: "max-content",
-                    }}
-                  ></lottie-player>
-                  <span className="contactText"> {navigationMenu[3]} </span>
-                </div>
-              </Link>
-            </li>
+           
           </ul>
 
           {/* Hamburger Menu for Mobile Device  */}
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,19 +1,14 @@
 "use client";
-import React, { useRef } from "react";
+import React from "react";
 import "../hero/hero.css";
 
 const Hero = ({ heroSection }) => {
-  const { title, subTitle, lottiePlayerURL } = heroSection;
+  const { title, subTitle } = heroSection;
 
   const firstTitle = title.substring(0, 19);
   const secondTitle = title.substring(20, 33);
   const thirdTitle = title.substring(34, 41);
-  console.log(secondTitle);
 
-  const ref = useRef(null);
-  React.useEffect(() => {
-    import("@lottiefiles/lottie-player");
-  });
 
   return (
     <div className="heroContainer">
@@ -29,15 +24,7 @@ const Hero = ({ heroSection }) => {
           <h3 className="shortDescription">{subTitle}</h3>
         </div>
         <div className="heroRight">
-          <lottie-player
-            ref={ref}
-            autoplay
-            loop
-            src={lottiePlayerURL}
-            speed="1"
-            style={{ width: "50rem", height: "auto", marginTop: "-5rem" }}
-            debug
-          ></lottie-player>
+        
         </div>
       </div>
     </div>
